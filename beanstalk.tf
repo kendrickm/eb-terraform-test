d9d39e7f0a52edae7b6f8ddcbf5db07b91b352ec
Kendrick Martin <kmartinix@gmail.com>
2019-11-14 15:38:18 -0800

    Adding beanstalk terraform code and updating gitignore to not include any tf metadata

---
+++ beanstalk.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+
+provider "aws" {
+  profile = "personal"
+  region  = "${var.aws_region}"
+  version = "~> 2.17.0"
+}
+
+resource "aws_elastic_beanstalk_application" "eb-demo" {
+  name        = "eb-demo"
+  description = "eb-demo"
+}
+
+resource "aws_elastic_beanstalk_environment" "eb-demo" {
+  name                = "eb-demo"
+  application         = "${aws_elastic_beanstalk_application.eb-demo.name}"
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.13.0 running Docker 18.06.1-ce"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = "arn:aws:iam::609831459651:instance-profile/aws-elasticbeanstalk-ec2-role"
+  }
+}
